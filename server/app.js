a7038b12f61020f5924ad79b7c538d3b83dcdba0
Raj Kumar <interplaytheheads@gmail.com>
2021-04-02 16:52:59 +0300

    Middleware uses

--- server/app.js
+++ server/app.js
@@ -2,10 +2,19 @@ const express = require('express')
 const app = express()
 const PORT = 5000
 
-
+const customMiddleware = (req, res, next) =>{
+    console.log("Middleware executed!!")
+    next()
+}
 
 app.get('/home', (req, res) => {
+    console.log("Home")
     res.send("Hello World")
+})
+
+app.get('/about',customMiddleware, (req, res) => {
+    console.log("about")
+    res.send("About Page.")
 })
 
 app.listen(PORT, ()=>{
