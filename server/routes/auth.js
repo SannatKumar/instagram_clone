e4df2280bbe806cff73bc2afc47ed561ed007dc2
Raj Kumar <interplaytheheads@gmail.com>
2021-04-03 18:55:26 +0300

    signup input validation and error defined

---
+++ server/routes/auth.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const router = express.Router()
+
+
+
+router.get('/', (req, res) =>{
+    res.send("Hello")
+})
+
+
+router.post('/signup', (req, res) =>{
+    const{name, email, password} = req.body
+    if(!email || !password || !name){
+        res.status(422).json({error: "Please add all the fields"})
+    }
+    res.json({message:"Successfully posted"})
+    console.log(req.body)
+})
+
+module.exports = router
