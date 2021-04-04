b1d6c20608211abb21a4c3080342065cac30db44
Raj Kumar <interplaytheheads@gmail.com>
2021-04-04 19:44:15 +0300

    signin route added and token sent as response

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,6 +3,8 @@ const router = express.Router()
 const mongoose = require('mongoose')
 const User = mongoose.model("User")
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const {JWT_SECRET} = require('../keys')
 
 
 router.post('/signup', (req, res) =>{
@@ -55,7 +57,9 @@ router.post('/signin', (req, res) =>{
         bcrypt.compare(password, savedUser.password)
         .then(doMatch =>{
             if(doMatch){
-                res.json({message: "Successfully Singed in"})
+                //res.json({message: "Successfully Singed in"})
+                const token = jwt.sign({_id:savedUser._id}, JWT_SECRET)
+                res.json({token})
             }
             else{
                 res.status(422).json({error: "Invalid Email or Password."})
