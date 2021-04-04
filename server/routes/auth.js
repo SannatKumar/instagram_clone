6c97a2fd436d57e87b0fb3c04882a8a9da059dea
Raj Kumar <interplaytheheads@gmail.com>
2021-04-04 19:16:23 +0300

    Password is hashed before saving

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,12 +2,7 @@ const express = require('express')
 const router = express.Router()
 const mongoose = require('mongoose')
 const User = mongoose.model("User")
-
-
-
-router.get('/', (req, res) =>{
-    res.send("Hello")
-})
+const bcrypt = require('bcryptjs')
 
 
 router.post('/signup', (req, res) =>{
@@ -21,20 +16,25 @@ router.post('/signup', (req, res) =>{
         if(savedUser){
             return res.status(422).json({error:"User already exists with that email"})
         }
-        const user = new User({
-            email,
-            password,
-            name
+        //hashing the password
+        bcrypt.hash(password,12)
+        .then(hashedPassword =>{
+            const user = new User({
+                email,
+                password:hashedPassword,
+                name
+            })
+            
+            //saving the user
+            user.save()
+            .then(user =>{
+                res.json({message:"Saved Successfully."})
+            })
+            .catch(err=>{
+                console.log(err)
+            })
         })
-        
-        //saving the user
-        user.save()
-        .then(user =>{
-            res.json({message:"Saved Successfully."})
-        })
-        .catch(err=>{
-            console.log(err)
-        })
+
     })
     .catch(err =>{
         console.log(err)
