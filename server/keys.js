0ca1f9ac55718fdbf4e34df24210796f0a889d11
Raj Kumar <interplaytheheads@gmail.com>
2021-04-04 19:42:17 +0300

    JWT_SECRET added to keys

--- server/keys.js
+++ server/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    MONGOURI: "mongodb+srv://rajkumar:Ramongodb123456..-@cluster0.hlrqo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    MONGOURI: "mongodb+srv://rajkumar:Ramongodb123456..-@cluster0.hlrqo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    JWT_SECRET:"effegtrejdjfdkk"
 }
