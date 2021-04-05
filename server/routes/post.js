3ec19e37b0ee114d3e1f069dc644d07af8069633
Raj Kumar <interplaytheheads@gmail.com>
2021-04-05 14:41:10 +0300

    routes defined for create post

--- server/routes/post.js
+++ server/routes/post.js
@@ -1,16 +1,25 @@
 const express = require('express')
 const router = express.Router()
 const mongoose = require('mongoose')
+const requireLogin = require('../middleware/requireLogin')
+const Post = mongoose.model("Post")
 
-router.post('./createpost', (req, res)=>{
+router.post('/createpost',requireLogin, (req, res)=>{
     const{title, body} = req.body
     if(!title || !body){
         res.status(422).json({error: "Please add all the fields."})
     }
-    const post = new post ({
+    const post = new Post ({
         title,
         body,
-        postedBy
+        postedBy:req.user,
+
+    })
+    post.save().then(result => {
+        res.json({post:result})
+    })
+    .catch(err =>{
+        console.log(err)
     })
 })
 module.exports = router
